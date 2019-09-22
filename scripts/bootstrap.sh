fa233e3c0e8013b917898b575adfd0b64e38d47d
Yang Wang <ywangd@gmail.com>
2019-09-22 21:48:38 +1000

    Add anaconda, virtualbox, hashicorp

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-[[ ! SKIP_INIT ]] && {
+[[ "$SKIP_INIT" == "0" || -z $SKIP_INIT ]] && {
+    echo 'Initialising'
     sudo apt update
-    sudo apt-get install -y software-properties-common
+    sudo apt-get install -y software-properties-common 
+    sudo apt-get install -y python-pexpect
     sudo apt-add-repository --yes --update ppa:ansible/ansible
     sudo apt-get install -y ansible
+    sudo apt --fix-broken install -y
 }
 pushd /tmp/raise-me-up/ansible
 ansible-galaxy install -r requirements.yml
